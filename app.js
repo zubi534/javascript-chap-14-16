d1b08c657ae7a22218ffe8345320981440470a28
zubi534 <81070198+zubi534@users.noreply.github.com>
2021-05-07 19:45:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+/* Q1 & 2*/
+
+/* var Array1 = [];
+var Array2 = new Array();
+ */
+
+
+
+/*Q3 */
+ /* var string =[ "zubaida" ,"kanwal"];
+ document.write(string); */
+
+
+ /* Q4 */
+ /* var no =[1,2,3];
+  document.write(no); */
+
+  /* Q5 */
+/* var bolean =[1,0];
+document.write(bolean); */
+
+/* Q6 */
+
+/* var mix =["zubaida", 5,];
+document.write(mix); */
+
+
+
+/* Q7 */
+
+/* var quali = ["SSC", "HSC", "BSC", "BS", "BCOM", "MS", "M.Phil", "Phd"];
+for(var i=0;i<=7;i++){
+
+    document.write("<p>"+(i+1)+")"+quali[i]+"</p>");
+} */
+
+/* Q8 */
+ 
+/* var name = [ "Michael", "john", "tony"];
+
+var name1_score = [320,230,480];
+
+var total_marks = 500 ;
+
+for(var i=0;i<=2;i++){
+
+    document.write("<p> scroes of "+name[i]+" is "+name1_score[i]+". Percentage: "+(name1_score[i]*100)/total_marks+ "%");
+} */
+
+
+/* Q10 */
+
+
+
+/* var temp;
+var score=[ 320,230,480,120];
+var order=[ 320,230,480,120];
+document.write("<p> Score of the students :"+score);
+for( i=0;i<=2;i++){
+   
+for(j=0;j<=2;j++)
+
+if(score[j]>=score[j+1])
+{ temp=score[j];
+    score[j]=score[j+1];
+    score[j+1]=temp;
+}
+}
+document.write("<p>Order Score of the students :"+score); */
+
+/* Q11 */
+
+
+/* var selected=[]
+var cities =["karachi","lahore","Islamabad","Quetta","Peshawar"];
+document.write(cities+"<br>");
+selected.push(cities[2]);
+selected.push(cities[3]);
+document.write(selected); */
+
+/* Q12 */
+
+
+/* var a =["This", "is", "my", "cat"];
+
+document.write(a.join(' ')); */
+
+
+
